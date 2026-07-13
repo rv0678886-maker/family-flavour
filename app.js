b2c8cf1a721f9f3f60d1db38860fc8289dfafcd3
rv0678886-maker <rv0678886@gmail.com>
2026-07-13 10:49:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+function placeOrder(){
+
+let name=document.getElementById("name").value;
+
+let phone=document.getElementById("phone").value;
+
+let address=document.getElementById("address").value;
+
+let landmark=document.getElementById("landmark").value;
+
+if(name==""||phone==""||address==""){
+
+alert("Please fill all details");
+
+return;
+
+}
+
+alert("Order Placed Successfully!\n\nCash on Delivery");
+
+localStorage.removeItem("cart");
+
+window.location="index.html";
+
+}
