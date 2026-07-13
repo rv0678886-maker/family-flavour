458edbb5c884e2476b2f620b1e34a8a933f134ec
rv0678886-maker <rv0678886@gmail.com>
2026-07-13 11:10:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkQNH2Hl-vxRETX4jjZH8rkCPbyxupA24",
@@ -15,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export { db , collection, addDoc };
