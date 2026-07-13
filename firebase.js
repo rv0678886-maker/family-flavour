9d6c326c2350b9ac740bc5101380c88c1737f7fd
rv0678886-maker <rv0678886@gmail.com>
2026-07-13 10:58:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
 
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "family-flavour.firebaseapp.com",
+  projectId: "family-flavour",
+  storageBucket: "family-flavour.firebasestorage.app",
+  messagingSenderId: "151508352233",
+  appId: "1:151508352233:web:6aa68d287c412476190473"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
